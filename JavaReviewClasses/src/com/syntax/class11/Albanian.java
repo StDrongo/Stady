33e8995ae372b4d44f3a054c61c536ec8f1492bc
Ilya <stdrongo@gmail.com>
2019-12-04 20:59:18 -0500

    some nationalities

--- JavaReviewClasses/src/com/syntax/class11/Albanian.java
+++ JavaReviewClasses/src/com/syntax/class11/Albanian.java
@@ -1,7 +1,30 @@
 package com.syntax.class11;
 
-public class Albanian extends Human{
+public class Albanian extends Human {
 	String albanianId;
+
+	public Albanian() {
+
+	}
+
+	public Albanian(String name, int age, int weight, String albanianId) {
+		super(name, age, weight);
+		this.albanianId = albanianId;
+	}
+
+	@Override
+	public String greeting() {
+		return "Pershendetje!";
+	}
+
+	@Override
+	public void talk() {
+		System.out.println("Une flas shqip. Une quhen " + name);
+	}
+	
+	public void albanianDance() {
+		System.out.println("Dancing like an albanian.");
+	}
 
 }
 
