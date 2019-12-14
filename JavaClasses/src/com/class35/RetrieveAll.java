2d92dac60bb801732faef1a4907f9f920ce46155
Ilya <stdrongo@gmail.com>
2019-12-14 12:38:00 -0500

    Map print 3 methods to print values

--- JavaClasses/src/com/class35/RetrieveAll.java
+++ JavaClasses/src/com/class35/RetrieveAll.java
@@ -15,21 +15,29 @@ public class RetrieveAll {
 		System.out.println(personMap);
 		Set<String> keys = personMap.keySet();
 		
-		//1st method of print. Line by line
+		//1st method of print "key". Line by line
 		for (String key:keys) {
 			System.out.println(key);
 		}
-		//2nd method of print. In array
+		//2nd method of print "key". In array
 		System.out.println(keys);
 		System.out.println("---------------");
+		//3rd method of print "key". Line by line
 		Iterator<String> keysIterator=keys.iterator();
 		while(keysIterator.hasNext()) {
 			System.out.println(keysIterator.next());
 		}
-		
-		
-		
-		
+		System.out.println("-------------");
+		Collection<String> values = personMap.values();
+		//1st method of print "value" line by line.
+		for(String val:values) {
+			System.out.println(val);
+		}
+		//2nd method of print "value" line by line
+		Iterator<String> valuesIterator=values.iterator();
+		while(valuesIterator.hasNext()) {
+			System.out.println(valuesIterator.next());
+		}
 		
 	}
 }
