8c1de9e3d4f00ae136c1930efe353b2b7d959037
Ilya <stdrongo@gmail.com>
2019-12-23 20:39:43 -0500

    some review with exceptions

--- Practice/src/morePractice/Main.java
+++ Practice/src/morePractice/Main.java
@@ -1,25 +1,24 @@
 package morePractice;
-//Complete The program and handle possible exception. 
-//Outputjava.lang.ArrayIndexOutOfBoundsException: 4
-//at Main.main(Main.java:5)
+//Complete the Code. 
+//Handle the possible Exception of Number format Exception. 
+//in Catch block print the Exception Class using getClass method. 
+//and print the message using getMessage method.
+//Output
+//class java.lang.NumberFormatException
+//For input string: "akki"
+
 
 
 
 
 
 class Main {
-public static void main(String[] args) throws InterruptedException{
-		
-	    int [] array = {10,20,30};
-	    System.out.println(array[4]);
-    try {
-    	main(args);
-    }
-    catch (IndexOutOfBoundsException e) {
-    	IndexOutOfBoundsException ind = new IndexOutOfBoundsException();
-		
-    	
-    }
-    
+	  public static void main(String[] args) {
+	    try {
+	    int num = Integer.parseInt ("akki");
+	  } catch(NumberFormatException e) {
+		  System.out.println(e.getClass());
+		  System.out.print(e.getMessage());
 	  }
+	}
 }
