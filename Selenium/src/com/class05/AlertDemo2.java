b7eb7e76cbc31ffa16250de924456dad233d1b4e
Ilya <stdrongo@gmail.com>
2019-12-29 12:20:02 -0500

    alert buttons

--- Selenium/src/com/class05/AlertDemo2.java
+++ Selenium/src/com/class05/AlertDemo2.java
@@ -5,8 +5,8 @@ import org.openqa.selenium.By;
 
 import com.utils.CommonMethods;
 
-public class AlertDemo1 extends CommonMethods{
-	
+public class AlertDemo2 extends CommonMethods{
+
 	public static final String SYNTAX_PRACTICE_URL = "http://166.62.36.207/syntaxpractice/index.html";
 
 	public static void main(String[] args) throws InterruptedException {
@@ -21,38 +21,19 @@ public class AlertDemo1 extends CommonMethods{
 		//finding Javascript Alerts and clicking on it
 		driver.findElement(By.linkText("Javascript Alerts")).click();
 		
-		//finding the button and click on it
-		driver.findElement(By.xpath("//button[@onclick='myAlertFunction()']")).click();
+		//finding the second alert button and click on it
+		driver.findElement(By.xpath("//button[@onclick='myConfirmFunction()']")).click();
 		Thread.sleep(2000);
-
+		
+		//handle the alert.
 		Alert alert=driver.switchTo().alert();
 		Thread.sleep(2000);
 
-		//get the text from alert box. before you hand the alert
-		System.out.println("Alert text:   "+alert.getText());
+		System.out.println("Text of Second Alert: "+alert.getText());
 		
-		//accept alert. any positive action. it could be OK, Yes, Accept, proceed.
-		alert.accept();
-		Thread.sleep(2000);
+		//to dismiss or cancel the alert. any negative action it will perform it
+		alert.dismiss();
 		
-		
-
-		//get the text from the ui or main window
-		String text=driver.findElement(By.xpath("//p[text()='Click the button to display an alert box:']")).getText();
-		System.out.println(text);
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		driver.close();
 	}
 }
--- Selenium/src/com/utils/CommonMethods.java
+++ Selenium/src/com/utils/CommonMethods.java
@@ -18,14 +18,16 @@ public class CommonMethods {
 	public static void setUp(String browser, String url){
 		
 		if(browser.equalsIgnoreCase("chrome")) {
-			System.setProperty("webdriver.chrome.driver", "drivers/chromeDriver");
+			System.setProperty("webdriver.chrome.driver", "drivers//chromeDriver.exe");
 			driver=new ChromeDriver();
 			driver.get(url);
 		
 		}else if(browser.equalsIgnoreCase("firefox")) {
-			System.setProperty("webdriver.gecko.driver", "drivers/geckodriver");
+			System.setProperty("webdriver.gecko.driver", "drivers//geckodriver.exe");
 			driver=new FirefoxDriver();
 			driver.get(url);
+		}else {
+			System.err.println("Browser not supported.");
 		}
 		
 	}
