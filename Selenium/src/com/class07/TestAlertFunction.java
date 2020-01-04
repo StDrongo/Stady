15e0827a06cbd9a08f54ff65361371b243182069
Ilya <stdrongo@gmail.com>
2020-01-04 13:06:31 -0500

    Wait..

--- Selenium/src/com/class07/TestAlertFunction.java
+++ Selenium/src/com/class07/TestAlertFunction.java
@@ -1,6 +1,5 @@
 package com.class07;
 
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 
 import com.utils.CommonMethods;
@@ -9,7 +8,7 @@ public class TestAlertFunction extends CommonMethods {
 	
 	public static void main(String[] args) throws InterruptedException {
 
-		setUp("chrome", "http://jiravm.centralus.cloudapp.azure.com:8081/javascript-alert-box-demo.html");
+		setUp("firefox", "http://jiravm.centralus.cloudapp.azure.com:8081/javascript-alert-box-demo.html");
 		driver.findElement(By.cssSelector("button[onclick='myConfirmFunction()']")).click();
 		
 		String alertText=getAlertText();
