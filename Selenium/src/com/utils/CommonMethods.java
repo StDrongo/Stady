5fecf4ffc65f5d21a1811befb5659aa279ee3ae2
Ilya <stdrongo@gmail.com>
2020-01-04 10:49:57 -0500

    some recap

--- Selenium/src/com/utils/CommonMethods.java
+++ Selenium/src/com/utils/CommonMethods.java
@@ -1,14 +1,14 @@
 package com.utils;
 
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class CommonMethods {
 	
-	
 	public static WebDriver driver;
-	
 	
 	/**
 	 * Use this method in need of opening browser and target url
@@ -18,19 +18,70 @@ public class CommonMethods {
 	public static void setUp(String browser, String url){
 		
 		if(browser.equalsIgnoreCase("chrome")) {
-			System.setProperty("webdriver.chrome.driver", "drivers//chromeDriver.exe");
+			System.setProperty("webdriver.chrome.driver", "drivers/chromeDriver");
 			driver=new ChromeDriver();
 			driver.get(url);
 		
 		}else if(browser.equalsIgnoreCase("firefox")) {
-			System.setProperty("webdriver.gecko.driver", "drivers//geckodriver.exe");
+			System.setProperty("webdriver.gecko.driver", "drivers/geckodriver");
 			driver=new FirefoxDriver();
 			driver.get(url);
 		}else {
-			System.err.println("Browser not supported.");
+			System.err.println("Browser not supported");
 		}
 		
 	}
+	
+	/**
+	 * This method will accept the alert
+	 * 
+	 * @throws NoAlertPresentException if alert is not present 
+	 */
+	public static void acceptAlert() {
+		
+		try {
+			Alert alert=driver.switchTo().alert();
+			alert.accept();
+		}catch (NoAlertPresentException e) {
+			System.out.println("Alert is not present");
+		}
+	}
+	
+	/**
+	 * This methods will dismiss the alert
+	 * 
+	 * @throws NoAlertPresentException if alert is not present 
+	 */
+	public static void dismissAlert() {
+		
+		try {
+			Alert alert=driver.switchTo().alert();
+			alert.dismiss();
+		}catch(NoAlertPresentException e) {
+			System.out.println("Alert is not prresent");
+		}
+	}
+	
+	/**
+	 * This method will get a text from the alert
+	 * 
+	 * @return text of the alert
+	 * @throws NoAlertPresentException if alert is not present
+	 */
+	
+	public static String getAlertText() {
+		
+		try {
+			Alert alert=driver.switchTo().alert();
+			return alert.getText();
+		}catch(NoAlertPresentException e) {
+			System.out.println("Alert is not present");
+			return null;
+		}
+	}
+	
+	
+	
 	
 //	public static WebDriver setUp(String browser){
 //		
