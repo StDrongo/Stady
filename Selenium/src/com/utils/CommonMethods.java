86fcf02188419eca11b838685d0c2d139481ef4e
Ilya <stdrongo@gmail.com>
2020-01-11 13:31:29 -0500

    file uploaded

--- Selenium/src/com/utils/CommonMethods.java
+++ Selenium/src/com/utils/CommonMethods.java
@@ -1,6 +1,9 @@
 package com.utils;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.Alert;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchFrameException;
 import org.openqa.selenium.WebDriver;
@@ -8,30 +11,34 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+//command+o--> for mac to see all methods within the class
+//ctrl+o--> for windows
 public class CommonMethods {
 
 	public static WebDriver driver;
 
+	/**
+	 * Use this method in need of opening browser and target url
+	 * 
+	 * @param browser The desired browser
+	 * @param url     The desired url
+	 */
 	public static void setUp(String browser, String url) {
 
 		if (browser.equalsIgnoreCase("chrome")) {
-
-			System.setProperty("webdriver.chrome.driver", "drivers//chromedriver.exe");
-
+			System.setProperty("webdriver.chrome.driver", "drivers/chromeDriver");
 			driver = new ChromeDriver();
-			driver.get(url);
-			driver.manage().window().maximize();
-
 		} else if (browser.equalsIgnoreCase("firefox")) {
-
-			System.setProperty("webdriver.gecko.driver", "drivers//geckodriver.exe");
-
+			System.setProperty("webdriver.gecko.driver", "drivers/geckodriver");
 			driver = new FirefoxDriver();
-			driver.get(url);
-			driver.manage().window().maximize();
 
+		} else {
+			System.err.println("Browser not supported");
 		}
-
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get(url);
 	}
 
 	/**
@@ -122,5 +129,55 @@ public class CommonMethods {
 			System.out.println("Frame is not present");
 		}
 	}
+
+	/**
+	 * This method will click on the element using JSExecutor
+	 * @param element
+	 */
+	public static void jsClick(WebElement element) {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].click();", element);
+	}
+
+	/**
+	 * This method will scroll until until specified element
+	 * @param element
+	 */
+	public static void scrollIntoElement(WebElement element) {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].scrollIntoView(true);", element);
+	}
+
+	/**
+	 * This method will scroll page down
+	 * @param pixel
+	 */
+	public static void scrollDown(int pixel) {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollBy(0," + pixel + ")");
+	}
+
+	/**
+	 * This method will scroll page up
+	 * @param pixel
+	 */
+	public static void scrollUp(int pixel) {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollBy(0, -" + pixel + ")");
+	}
+
+//	public static WebDriver setUp(String browser){
+//		
+//		if(browser.equalsIgnoreCase("chrome")) {
+//			System.setProperty("webdriver.chrome.driver", "drivers/chromeDriver");
+//			driver=new ChromeDriver();
+//		
+//		}else if(browser.equalsIgnoreCase("firefox")) {
+//			System.setProperty("webdriver.gecko.driver", "drivers/geckodriver");
+//			driver=new FirefoxDriver();
+//		}
+//		
+//		return driver;
+//	}
 
 }
